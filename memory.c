ffd74d299dd919f82f81126634b1d27529e01065
uhhrye <58236573+uhhrye@users.noreply.github.com>
2019-12-03 09:02:42 -0800

    Create memory.c

---
+++ memory.c
@@ -0,0 +1,10 @@
+// PROJ 9 FINAL PROJECT
+// MEMORY GAME
+// A program that ______
+// V0.1
+// DEVS: RYAN ALMAZAN, ULYSSES RIVERA, DEL MYER
+
+#include <stdio.h>
+#include <string.h>
+#define MAX_STR 100
+#define MAX_NAME 15
