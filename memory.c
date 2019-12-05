1f89cf1467645c104178c63d207cc8e2c9df6c60
uhhrye <58236573+uhhrye@users.noreply.github.com>
2019-12-05 14:07:39 -0800

    Update memory.c

--- memory.c
+++ memory.c
@@ -29,6 +29,9 @@ int main()
 	FILE *fp;
 	int choice, difficulty, scoreboard[10] = NULL;
 	
+	int x=0, y=0, a=0, b=0;
+	coordinate( &x,  &y, &a, &b);
+	
 	//seed the "random" number generator with NULL
     	srand(time(NULL));
 
@@ -143,51 +146,37 @@ void outputnames(FILE*fp, char array[][10], int score[])
 		}
 
 }
-// copy and pasted from my code-Ulysses
-void coordinate( int *x, int *y, int *a ,  int *b);
-_Bool duplicate( int x, int y, int a, int b);
-void getcoord(int *x,int *y);
-int main()
-{
-int x=0, y=0, a=0, b=0;
-	coordinate( &x,  &y, &a, &b);
-	
-
-return 0;
-}
 
 void coordinate( int *x, int *y, int *a, int *b)
 {
-int dup= 0;
+	_Bool dup = 0;
 
 	getcoord(x,y);
 	getcoord(a,b);
 	dup = duplicate(*x,*y,*a,*b);
-	while (dup )
+	while(dup)
 	{
-		printf("duplicate\n");
+		printf("Duplicate\n");
 		getcoord(a,b);
 		dup = duplicate(*x,*y,*a,*b);
 	}
 	
 }
-void getcoord(int *x, int*y)
-{
 
+void getcoord(int *x, int *y)
+{
 	printf("Enter your coordinates\n");
 	scanf("%d\t%d",x,y);
-
 }
 
 
-_Bool duplicate( int x, int y, int a, int b)
+_Bool duplicate(int x, int y, int a, int b)
 {
 	if(x ==a && y==b)
 	{
 		return 1;
 	}
-return 0;
-		
+	return 0;
 }
 
 //declare global variables
@@ -195,95 +184,89 @@ return 0;
 //define functions
 void prefill_board (int board_size, char game_board[board_size][board_size])
 {
-    for(int row_index = 0; row_index < board_size; row_index++)
-    {
-        for(int col_index = 0; col_index < board_size; col_index++)
-        {
-            game_board[row_index][col_index] = 'X';
-        }   
-    }
+	for(int row_index = 0; row_index < board_size; row_index++)
+	{
+		for(int col_index = 0; col_index < board_size; col_index++)
+		{
+			game_board[row_index][col_index] = 'X';
+		}   
+	}
 
-    //end function
-    return;
+	//end function
 }
 
 void fill_board_with_pairs(int difficulty, int board_size, char game_board[board_size][board_size])
 {
-    //declare local variables
-    int row = 0, column = 0, number_pairs = 0, counter = 0;
-    char pair_symbol = 'A';
-    _Bool pair = 1;
+	//declare local variables
+	int row = 0, column = 0, number_pairs = 0, counter = 0;
+	char pair_symbol = 'A';
+	_Bool pair = 1;
     
-    //controlling expression for previous looping attempt below (commented out)
-    number_pairs = (difficulty * board_size);
+	//controlling expression for previous looping attempt below (commented out)
+	number_pairs = (difficulty * board_size);
 
-    //new idea for assigning pairs of "randomly" generated symbols, in tandem order (must be shuffled)
-    for(int row_index = 0; row_index < board_size; row_index++)
-    {
-        for(int col_index = 0; col_index < board_size; col_index++)
-        {
-            game_board[row_index][col_index] = random_symbol_generator();
-            pair_symbol = game_board[row_index][col_index];
-            col_index++;
-            game_board[row_index][col_index] = pair_symbol;
-        }   
-    }
+	//new idea for assigning pairs of "randomly" generated symbols, in tandem order (must be shuffled)
+	for(int row_index = 0; row_index < board_size; row_index++)
+	{
+		for(int col_index = 0; col_index < board_size; col_index++)
+		{
+			game_board[row_index][col_index] = random_symbol_generator();
+			pair_symbol = game_board[row_index][col_index];
+			col_index++;
+			game_board[row_index][col_index] = pair_symbol;
+		}
+	}
 
-    //This should work, and almost does; upon testing. Find out why is does not.
-    /*
-    do
-    {
-        row = random_number_generator(board_size - 1);
-        column = random_number_generator(board_size - 1);
-        printf("1\n");
-        if(game_board[row][column] == 'X')
-        {
-            printf("2\n");
-            game_board[row][column] = random_symbol_generator();
-            pair_symbol = game_board[row][column];
-            pair = 1;
-            while(pair)
-            {
-                printf("3\n");
-                row = random_number_generator(board_size - 1);
-                column = random_number_generator(board_size - 1);
-                if(game_board[row][column] == 'X')
-                {
-                    printf("4\n");
-                    game_board[row][column] = pair_symbol;
-                    pair = 0;
-                    counter++;
-                }
-            }
-        }
-        
-    } while(counter <= number_pairs);
+	//This should work, and almost does; upon testing. Find out why is does not.
+	/*
+	do
+	{
+		row = random_number_generator(board_size - 1);
+		column = random_number_generator(board_size - 1);
+		printf("1\n");
+		if(game_board[row][column] == 'X')
+		{
+			printf("2\n");
+			game_board[row][column] = random_symbol_generator();
+			pair_symbol = game_board[row][column];
+			pair = 1;
+			while(pair)
+			{
+				printf("3\n");
+				row = random_number_generator(board_size - 1);
+				column = random_number_generator(board_size - 1);
+				if(game_board[row][column] == 'X')
+				{
+					printf("4\n");
+					game_board[row][column] = pair_symbol;
+					pair = 0;
+					counter++;
+				}
+			}
+		}
+	}while(counter <= number_pairs);
     */
-
-    return;
 }
 
 void shuffle_game_board(int difficulty, int board_size, char game_board[board_size][board_size])
 {
-    int row = 0, column = 0;
-    char pair_symbol1  = 'A', pair_symbol2 = 'A';
+	int row = 0, column = 0;
+	char pair_symbol1  = 'A', pair_symbol2 = 'A';
 
-    for(int index = 0; index < 3; index++)
-    {
-    for(int row_index = 0; row_index < board_size; row_index++)
-    {
-        for(int col_index = 0; col_index < board_size; col_index++)
-        {
-            pair_symbol1 = game_board[row_index][col_index];
-            row = random_number_generator(board_size);
-            column = random_number_generator(board_size);
-            game_board[row_index][col_index] = game_board[row][column];
-            game_board[row][column] = pair_symbol1;
-            
-        }   
-    }
-    }
-    return;
+	for(int index = 0; index < 3; index++)
+	{
+		for(int row_index = 0; row_index < board_size; row_index++)
+		{
+			for(int col_index = 0; col_index < board_size; col_index++)
+			{
+				pair_symbol1 = game_board[row_index][col_index];
+				row = random_number_generator(board_size);
+				column = random_number_generator(board_size);
+				game_board[row_index][col_index] = game_board[row][column];
+				game_board[row][column] = pair_symbol1;
+			}
+		}
+	}
 }
 
 int random_number_generator(int board_size)
